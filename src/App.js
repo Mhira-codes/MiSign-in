d1a1953dc30ab3954a37784a8caaf9734139e8a2
mhira-codes <alhassansamiratu991@gmail.com>
2022-08-11 12:58:58 +0000

    updated app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './Login';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 
 function App() {
   return (
     <div className="App">
-  <Login/>
-  
+      <Routes>
+        <Route element={<Login/>} path="/" />
+        <Route element={<Signup />} path="/signup" />
+      </Routes>
+
     </div>
   );
 }
